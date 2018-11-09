1595b003afc9c9c001ae932538f1a3c91e82513f
osReilly <ozreilly@163.com>
2018-11-09 16:28:34 +0800

    fdf

--- test.js
+++ test.js
@@ -1,10 +1,41 @@
-const Koa = require('koa');
-const app = new Koa();
+var app = require('koa')()
+  , logger = require('koa-logger')
+  , json = require('koa-json')
+  , views = require('koa-views')
+  , onerror = require('koa-onerror');
 
-app.use(async ctx => {
-  ctx.body = 'Hello World<br>我是一个koa小demo';
+var index = require('./routes/index');
+var users = require('./routes/users');
+var mongoose = require('mongoose')
+// error handler
+onerror(app);
+
+// global middlewares
+app.use(views('views', {
+  root: __dirname + '/views',
+  default: 'jade'
+}));
+app.use(require('koa-bodyparser')());
+app.use(json());
+app.use(logger());
+
+app.use(function *(next){
+  var start = new Date;
+  yield next;
+  var ms = new Date - start;
+  console.log('%s %s - %s', this.method, this.url, ms);
 });
 
-app.listen(4100,()=>{
-  console.log('start nodejs server port in'+4100)
+app.use(require('koa-static')(__dirname + '/public'));
+app.use(users.routes(), users.allowedMethods());
+// routes definition
+app.use(index.routes(), index.allowedMethods());
+
+// error-handling
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx)
+});
+let port = 4100
+app.listen(port, () => {
+  console.log(`Server started on ${port}`);
 });
